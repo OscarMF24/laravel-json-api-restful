91e16f28efd6ad99d940a5ece4576b135a2f28ba
Oscar Franco <franco.touches@gmail.com>
2024-02-20 08:18:28 -0600

    refactor: :construction: Response from articles show with resources

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,27 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Article;
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
 
 class ArticleController extends Controller
 {
-    public function show(Article $article): JsonResponse
+    public function show(Article $article): ArticleResource
     {
-        return new JsonResponse([
-            'data' => [
-                'type' => 'articles',
-                'id' => (string) $article->getRouteKey(),
-                'attribute' => [
-                    'title' => $article->title,
-                    'slug' => $article->slug,
-                    'content' => $article->content
-                ],
-                'links' => [
-                    'self' => route('api.v1.articles.show', $article)
-                ]
-            ]
-        ]);
+        return ArticleResource::make($article);
     }
 }
