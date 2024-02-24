6aff1687226d0081002c596f959f344d9b36a250
Oscar Franco <franco.touches@gmail.com>
2024-02-23 18:34:02 -0600

    Test for validate slug

---
+++ app/Rules/Slug.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Slug implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if ($this->hasUnderscores($value)) $fail('validation.no_underscores')->translate();
+
+        if ($this->startsWithDashes($value)) $fail('validation.no_starting_dashes')->translate();
+
+        if ($this->endsWithDashes($value)) $fail('validation.no_ending_dashes')->translate();
+    }
+
+    /**
+     * @param $value
+     * @return false|int
+     */
+    protected function hasUnderscores($value)
+    {
+        return preg_match('/_/', $value);
+    }
+
+    /**
+     * @param $value
+     * @return false|int
+     */
+    protected function startsWithDashes($value)
+    {
+        return preg_match('/^-/', $value);
+    }
+
+    /**
+     * @param $value
+     * @return false|int
+     */
+    protected function endsWithDashes($value)
+    {
+        return preg_match('/-$/', $value);
+    }
+}
